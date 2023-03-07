b25ab57602adb2cbe176282e0b99e36edd1430e2
vishesh <visheshtiwari211@gmail.com>
2023-03-07 19:38:35 +0530

    feat: created LastFmRepository

--- app/src/main/java/com/example/lastfmapi/MainActivity.kt
+++ app/src/main/java/com/example/lastfmapi/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.lastfmapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.lastfmapi.api.LastFmInstance
+import com.example.lastfmapi.viewmodel.LastFmViewModel
+import com.example.lastfmapi.viewmodel.LastFmViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: LastFmViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lastFmApi = LastFmInstance.lastFmApi
+        val viewModelFactory = LastFmViewModelFactory(lastFmApi)
+
+        viewModel = ViewModelProvider(this, viewModelFactory)[LastFmViewModel::class.java]
+
+        viewModel.getTopTags()
+
     }
 }
---
+++ app/src/main/java/com/example/lastfmapi/repository/LastFmRepository.kt
@@ -0,0 +1,4 @@
+package com.example.lastfmapi.repository
+
+class LastFmRepository {
+}

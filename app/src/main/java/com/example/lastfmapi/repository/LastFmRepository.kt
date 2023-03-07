8c43d53abd5dbcd23ecb3626b2ea2794958a86fd
vishesh <visheshtiwari211@gmail.com>
2023-03-07 19:49:29 +0530

    feat: added function in LastFmRepository

--- app/src/main/java/com/example/lastfmapi/repository/LastFmRepository.kt
+++ app/src/main/java/com/example/lastfmapi/repository/LastFmRepository.kt
@@ -1,4 +1,12 @@
 package com.example.lastfmapi.repository
 
+import com.example.lastfmapi.api.LastFmApi
+import com.example.lastfmapi.api.LastFmInstance
+import com.example.lastfmapi.dataclasses.GetTopTags
+import retrofit2.Response
+
 class LastFmRepository {
+    suspend fun getGenres(): Response<GetTopTags> {
+        return LastFmInstance.lastFmApi.getGenres()
+    }
 }

c2c192d3688c85a4b2302df7538a29d791a51446
vishesh <visheshtiwari211@gmail.com>
2023-03-07 19:51:25 +0530

    feat: added repository dependency in LastFmViewModel

--- app/src/main/java/com/example/lastfmapi/viewmodel/LastFmViewModel.kt
+++ app/src/main/java/com/example/lastfmapi/viewmodel/LastFmViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.lastfmapi.viewmodel
 
-class LastFmViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.lastfmapi.repository.LastFmRepository
+
+class LastFmViewModel(private val LastFmRepository: LastFmRepository): ViewModel() {
+
+//    fun getTopTags() {
+//        viewModelScope.launch {
+//            val response = LastFmApi.getGenres().body()
+//            if (response?.toptags?.tag?.isNotEmpty() == true){
+//                response.toptags.tag.forEach {
+//                    println(it.name)
+//                }
+//            } else{
+//                Log.e("LastFmApi", "Api not working. List size zero!!!!!")
+//            }
+//        }
+//    }
 }

3740284a31cb64380d9fc6cc2066b0acb95db706
vishesh <visheshtiwari211@gmail.com>
2023-03-11 23:21:10 +0530

    fix: getGenreInfo in viewmodel

--- app/src/main/java/com/example/lastfmapi/viewmodel/LastFmViewModel.kt
+++ app/src/main/java/com/example/lastfmapi/viewmodel/LastFmViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.lastfmapi.dataclasses.Tag
 import com.example.lastfmapi.dataclasses.TagX
 import com.example.lastfmapi.repository.LastFmRepository
 import kotlinx.coroutines.launch
@@ -12,11 +13,23 @@ class LastFmViewModel(private val LastFmRepository: LastFmRepository): ViewModel
 
     private val _topTags: MutableLiveData<List<TagX>> = MutableLiveData()
     val topTags: LiveData<List<TagX>> = _topTags
+
+    private val _tagInfo: MutableLiveData<Tag> = MutableLiveData()
+    val tagInfo: LiveData<Tag> = _tagInfo
     fun getTopTags() {
         viewModelScope.launch {
             val response = LastFmRepository.getGenres()
             if (response.isSuccessful){
                 _topTags.value = response.body()?.toptags?.tag
+            }
+        }
+    }
+
+    fun getGenreInfo(tag: String) {
+        viewModelScope.launch {
+            val response = LastFmRepository.getGenreInfo(tag)
+            if (response.isSuccessful) {
+                _tagInfo.value = response.body()?.tag
             }
         }
     }

9a99a59ea633be81c3afe18c88f3df52591377c3
vishesh <visheshtiwari211@gmail.com>
2023-03-11 17:19:24 +0530

    feat: added viewBinding in gradle and MainActivity

--- app/src/main/java/com/example/lastfmapi/MainActivity.kt
+++ app/src/main/java/com/example/lastfmapi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lastfmapi
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.lastfmapi.databinding.ActivityMainBinding
 import com.example.lastfmapi.repository.LastFmRepository
 import com.example.lastfmapi.viewmodel.LastFmViewModel
 import com.example.lastfmapi.viewmodel.LastFmViewModelFactory
@@ -10,9 +11,14 @@ import com.example.lastfmapi.viewmodel.LastFmViewModelFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: LastFmViewModel
+    private lateinit var binding: ActivityMainBinding
+    private var isListExpanded = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        createOrRefreshList()
 
         val lastFmRepository = LastFmRepository()
         val viewModelFactory = LastFmViewModelFactory(lastFmRepository)
@@ -20,8 +26,14 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this, viewModelFactory)[LastFmViewModel::class.java]
 
         viewModel.topTags.observe(this) {
-            
+            createOrRefreshList()
         }
 
+    }
+
+    private fun createOrRefreshList() {
+        if (isListExpanded) {
+
+        }
     }
 }

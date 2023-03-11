027cae42268dd2eec1a1682eee49aa0f300a17ed
vishesh <visheshtiwari211@gmail.com>
2023-03-11 22:47:04 +0530

    feat: added getGenreInfo in Repository

--- app/src/main/java/com/example/lastfmapi/repository/LastFmRepository.kt
+++ app/src/main/java/com/example/lastfmapi/repository/LastFmRepository.kt
@@ -1,11 +1,16 @@
 package com.example.lastfmapi.repository
 
 import com.example.lastfmapi.api.LastFmInstance
+import com.example.lastfmapi.dataclasses.GetTagInfo
 import com.example.lastfmapi.dataclasses.GetTopTags
 import retrofit2.Response
 
 class LastFmRepository {
     suspend fun getGenres(): Response<GetTopTags> {
         return LastFmInstance.lastFmApi.getGenres()
+    }
+
+    suspend fun getGenreInfo(tag: String): Response<GetTagInfo> {
+        return LastFmInstance.lastFmApi.getGenreInfo(tag = tag)
     }
 }

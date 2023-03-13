477bc32f1c86f896923d9cecb5889e0d36b7abfa
vishesh <visheshtiwari211@gmail.com>
2023-03-13 19:11:37 +0530

    feat: created ArtistsAdapter.kt and TracksAdapter.kt

---
+++ app/src/main/java/com/example/lastfmapi/adapters/TracksAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.lastfmapi.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lastfmapi.dataclasses.Track
+
+class TracksAdapter(val list: MutableList<Track>): RecyclerView.Adapter<TracksAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}

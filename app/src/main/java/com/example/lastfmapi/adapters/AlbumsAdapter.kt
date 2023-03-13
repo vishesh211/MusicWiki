5221d8e14176ad6e81b0cfee997aefe02d7337b8
vishesh <visheshtiwari211@gmail.com>
2023-03-13 19:09:25 +0530

    feat: implemented adapters functionality in AlbumsAdapter.kt

--- app/src/main/java/com/example/lastfmapi/adapters/AlbumsAdapter.kt
+++ app/src/main/java/com/example/lastfmapi/adapters/AlbumsAdapter.kt
@@ -1,4 +1,22 @@
 package com.example.lastfmapi.adapters
 
-class AlbumsAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lastfmapi.dataclasses.Track
+
+class AlbumsAdapter(val list: MutableList<Track>): RecyclerView.Adapter<AlbumsAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }

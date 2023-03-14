133d0282ae31391ef97f49cf75f2ceb3b0a128f0
vishesh <visheshtiwari211@gmail.com>
2023-03-14 18:01:52 +0530

    feat: implemented getTagTopArtists in api

--- app/src/main/java/com/example/lastfmapi/api/LastFmApi.kt
+++ app/src/main/java/com/example/lastfmapi/api/LastFmApi.kt
@@ -36,4 +36,12 @@ interface LastFmApi {
         @Query("format") format: String = "json"
     ): Response<GetTopAlbums>
 
+    @GET("2.0/")
+    suspend fun getTagTopArtists(
+        @Query("method") method: String = "tag.gettopartists",
+        @Query("tag") tag: String,
+        @Query("api_key") api_key: String = "3ec4da6005c36f71fdc94b95f46869d6",
+        @Query("format") format: String = "json"
+    )
+
 }

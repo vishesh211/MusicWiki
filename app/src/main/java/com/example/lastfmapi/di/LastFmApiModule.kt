23098b3c2f758950edc0d6ed6f44dccd3cd60025
vishesh211 <visheshtiwari211@gmail.com>
2023-03-26 17:31:38 +0530

    feat: added LastFmApi in module

--- app/src/main/java/com/example/lastfmapi/di/LastFmApiModule.kt
+++ app/src/main/java/com/example/lastfmapi/di/LastFmApiModule.kt
@@ -1,4 +1,20 @@
 package com.example.lastfmapi.di
 
+import com.example.lastfmapi.api.LastFmApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object LastFmApiModule {
+
+    @Provides
+    @Singleton
+    fun provideLastFmApi(
+        retrofit: Retrofit
+    ): LastFmApi = retrofit.create(LastFmApi::class.java)
 }

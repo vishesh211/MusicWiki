d6918568c7cb2fc3951738ba976a56a07e23c6d5
vishesh211 <visheshtiwari211@gmail.com>
2023-04-08 23:23:17 +0530

    fix: added ArtistDetailActivity

--- app/src/main/java/com/example/lastfmapi/ui/ArtistDetailActivity.kt
+++ app/src/main/java/com/example/lastfmapi/ui/ArtistDetailActivity.kt
@@ -1,12 +1,41 @@
 package com.example.lastfmapi.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lastfmapi.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.lastfmapi.adapters.AlbumInfoGenreAdapter
+import com.example.lastfmapi.databinding.ActivityArtistDetailBinding
+import com.example.lastfmapi.dataclasses.TagXX
+import com.example.lastfmapi.viewmodel.LastFmViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ArtistDetailActivity : AppCompatActivity() {
+
+    private val viewModel: LastFmViewModel by viewModels()
+
+    private lateinit var binding: ActivityArtistDetailBinding
+
+    var genreNameList: MutableList<TagXX> = mutableListOf()
+    private val albumGenreInfoAdapter: AlbumInfoGenreAdapter = AlbumInfoGenreAdapter(genreNameList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_artist_detail)
+        binding = ActivityArtistDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val artist = intent.getStringExtra("artist")
+
+        viewModel.getArtistInfo(artist.toString())
+
+        viewModel.artistInfo.observe(this) {
+            Glide.with(this).load(it.image[0].text).into(binding.artistInfoImage)
+            binding.artistInfoPlayCountNumber.text = it.stats.playcount
+            binding.artistInfoFollowerNumber.text = it.stats.listeners
+            genreNameList.addAll(it.tags.tag)
+            binding.artistInfoRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            binding.artistInfoRv.adapter = albumGenreInfoAdapter
+        }
     }
 }

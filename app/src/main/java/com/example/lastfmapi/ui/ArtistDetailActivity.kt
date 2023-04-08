1bd2e3ef69fb7eceeaa75889cc11705d11df41f7
vishesh211 <visheshtiwari211@gmail.com>
2023-04-08 18:50:04 +0530

    fix: created ArtistDetailActivity

---
+++ app/src/main/java/com/example/lastfmapi/ui/ArtistDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lastfmapi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lastfmapi.R
+
+class ArtistDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_detail)
+    }
+}

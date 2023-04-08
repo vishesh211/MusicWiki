fdddfbd1158e736ae58bc06ff7cb7e5d5c31e1dd
vishesh211 <visheshtiwari211@gmail.com>
2023-04-08 17:02:41 +0530

    fix: inserted viewmodel in activity

--- app/src/main/java/com/example/lastfmapi/ui/AlbumDetailActivity.kt
+++ app/src/main/java/com/example/lastfmapi/ui/AlbumDetailActivity.kt
@@ -1,12 +1,35 @@
 package com.example.lastfmapi.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lastfmapi.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.lastfmapi.databinding.ActivityAlbumDetailBinding
+import com.example.lastfmapi.viewmodel.LastFmViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AlbumDetailActivity : AppCompatActivity() {
+
+    private val viewModel: LastFmViewModel by viewModels()
+
+    private lateinit var binding: ActivityAlbumDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_album_detail)
+        binding = ActivityAlbumDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+        val albumName = intent.getStringExtra("album name")
+        val artistName = intent.getStringExtra("artist name")
+        viewModel.getAlbumInfo(album = albumName.toString(), artist = artistName.toString())
+
+        binding.albumInfoName.text = albumName.toString()
+        binding.albumArtistInfoName.text = artistName.toString()
+
+        viewModel.albumInfo.observe(this) {
+            Glide.with(this).load(it.image[0].text).into(binding.albumImage)
+            binding.albumInfoAlbumDetail.text = it.wiki.summary
+        }
     }
 }
